0a0c05a093f4ddbc9f0eddd4fca8945957c6642c
Robin Lemmers <38210@ma-web.nl>
2024-04-11 09:56:46 +0200

    fifth-card / chartjs

---
+++ main.js
@@ -0,0 +1,25 @@
+const ctx = document.getElementById('myChart');
+
+let data = {
+    labels: ["jan", "feb", "mar", "apr", "mei", "jun"],
+    datasets: [{
+        label: "Verbruik (KwH)",
+        data: [30, 20, 25, 10, 35, 10],
+        backgroundColor: ["#EEEEEE"],
+        borderColor: "#000000",
+        borderWidth: 1
+    }]
+}
+
+let config = {
+    type: "bar",
+    data: data,
+    options: {
+        maintainAspectRatio: false,
+        y: {
+            max: 60
+        }
+    }
+}
+
+let myChart = new Chart(ctx, config)
